c5700f445db6a98199cef2c4346d0c6ddab0efa8
Vuyo Nkadimeng <zimasilevuyo@gmail.com>
2020-09-02 10:31:12 +0200

    Update user.py
    Update the encryption for user data including password and username.

--- hammer/user.py
+++ hammer/user.py
@@ -21,9 +21,27 @@ return null
 
 def encrypt():
 
-	"""
-	Encrypts User's data and protects it. 
-	"""
+	alphabets = "abcdefghijklmnopqrstuvwxyz"
+			characters = "abcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+`~|][;'>?/<{}"
+
+			# Key value to generate new username
+			key_name = 5
+			# Key value to generate new password
+			key_password = 7
+			encrypt_name = ""
+			encrypt_password = ""
+
+			for i in self.name:
+				position_name = alphabets.find(i)
+				newPosition_name = (position_name+key_name) % 26
+				# slicing alphabets
+				encrypt_name += alphabets[newPosition_name]
+
+			for i in self.password:
+				position_password = characters.find(i)
+				newPosition_password = (position_password+key_password) % 62
+				encrypt_password += characters[newPosition_password]
+
 	null = 0
 return null
 
